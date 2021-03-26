891e4f0539e6f58c216239ab50174e2035f29636
Kunal Singh <kunal.singh_cs18@gla.ac.in>
2021-03-26 21:44:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const express=require('express');
+const app=express()
+const PORT=process.env.PORT || 3000
+const ejs=require('ejs')
+const path=require('path')
+const expressLayout=require('express-ejs-layouts')
+const mongoose=require('mongoose')
+const session=require('express-session')
+const flash=require('express-flash')
+const MongoDbStore=require('connect-mongo')(session)
+
+
+
+
+// Database Cpnnection
+const url='mongodb://localhost/pizz360'
+mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log("Database connected..");
+}).catch(err=>{
+    console.log("Connection failed...");
+});
+
+//Session store
+
+ let mongoStore=new MongoDbStore({
+    mongooseConnection:connection,
+    collection:'sessions'
+ })
+
+//Session config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    store:mongoStore,
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24}
+}))
+
+app.use(flash())
+
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()
+})
+//Assets
+app.use(express.static('public'));
+//set templates engine
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+require('./routes/web')(app)
+
+
+app.listen(PORT,()=>{
+    console.log("Listen on port 3000");
+})
